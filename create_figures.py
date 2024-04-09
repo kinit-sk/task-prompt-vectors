c396d07c242cda30b66ac5d42cebdd697abe6bf2
Robert Belanec <belanecrobert22@gmail.com>
2024-04-09 14:00:18 +0000

    updated create_figures

--- create_figures.py
+++ create_figures.py
@@ -18,12 +18,12 @@ for t in df["tasks"]:
 
 print(res_dict)
 
-data = {"first task": [], "second_task": []}
+data = {"first_task": [], "second_task": []}
 
 for t in res_dict:
     if len(t.split(" ")) == 2:
         first_task, second_task = t.split(" ")
-        data["first task"].append(res_dict[t][first_task])
+        data["first _task"].append(res_dict[t][first_task])
         data["second_task"].append(res_dict[t][second_task])
 
 print(data)
