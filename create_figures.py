8d431c631b3418996874153e471e41bad4a3d8a3
Robert Belanec <belanecrobert22@gmail.com>
2024-04-18 15:30:56 +0000

    added results

--- create_figures.py
+++ create_figures.py
@@ -3,40 +3,62 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("results_04102024083442.csv", index_col=0)
-df = df.groupby(["tasks"], as_index=False).first() # comment when fixed
+import glob
 
-res_dict = {}
 
-for t in df["tasks"]:
-    print(t)
-    for tt in t.split(" "):
-        if t not in res_dict:
-            res_dict[t] = {}
-        
-        res_dict[t].update({tt: df[df["tasks"] == t][f"{tt}_accuracy"].values[0]/df[df["tasks"] == tt][f"{tt}_accuracy"].values[0]})
+dfs = []
 
-print(res_dict)
+for file in sorted(glob.glob("results_origin*.csv")):
 
-data = {"first_task": [], "second_task": [], "tasks": []}
+    df = pd.read_csv(file, index_col=0)
 
-for t in res_dict:
-    if len(t.split(" ")) == 2:
-        first_task, second_task = t.split(" ")
-        data["first_task"].append(res_dict[t][first_task])
-        data["second_task"].append(res_dict[t][second_task])
-        data["tasks"].append(t)
+    df["tasks"] = df["tasks"].map(lambda x: " ".join(sorted(x.split(" "))))
 
-data = pd.DataFrame.from_dict(data)
-print(data)
+    dfs.append(df)
 
-scatterplot = sns.scatterplot(data=data, x="first_task", y="second_task", hue="tasks", style="tasks")
+mean_dfs = pd.concat(dfs).groupby("tasks",as_index=False).mean()
+mean_dfs_std = pd.concat(dfs).groupby("tasks",as_index=False).std()
+
+mean_dfs.to_csv("average_10.csv")
+mean_dfs_std.to_csv("std_10.csv")
+
+data_dfs = []
+
+for df in [mean_dfs, mean_dfs_std]:
+    res_dict = {}
+
+    for t in df["tasks"]:
+        print(t)
+        for tt in t.split(" "):
+            if t not in res_dict:
+                res_dict[t] = {}
+            
+            res_dict[t].update({tt: df[df["tasks"] == t][f"{tt}_accuracy"].values[0]/mean_dfs[df["tasks"] == tt][f"{tt}_accuracy"].values[0]})
+
+    print(res_dict)
+
+    data = {"first_task": [], "second_task": [], "tasks": []}
+
+    for t in res_dict:
+        if len(t.split(" ")) == 2:
+            first_task, second_task = t.split(" ")
+            data["first_task"].append(res_dict[t][first_task])
+            data["second_task"].append(res_dict[t][second_task])
+            data["tasks"].append(t)
+
+    data = pd.DataFrame.from_dict(data)
+
+    print(data)
+    data_dfs.append(data)
+
+scatterplot = sns.scatterplot(data=data_dfs[0], x="first_task", y="second_task", hue="tasks", style="tasks")
                 
 plt.vlines(x=1, ymin=0, ymax=1.2, colors='gray',linestyles='dashed')
 plt.hlines(y=1, xmin=0, xmax=1.2, colors='gray',linestyles='dashed')
 scatterplot.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
 fig = scatterplot.get_figure()
-fig.savefig("results_04102024083442.png", bbox_inches='tight') 
+fig.savefig("results_10.png", bbox_inches='tight') 
 
-data.to_csv("data_results_04102024083442.csv")
+data_dfs[0].to_csv("data_results_10.csv")
+data_dfs[1].to_csv("data_results_std_10.csv")
